ac851e87482502bb8b6166bf81ec3011712a60cd
Dawid Femin <w65311@student.wsiz.edu.pl>
2022-04-16 01:04:57 +0200

    Completed Leagues component

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import '../style/Leagues.css';
 
 const Leagues = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios('https://api-football-standings.azharimm.site/leagues')
+    .then(response => {
+      console.log(response.data.data)
+      setData(response.data.data)
+    })
+  },[]);
+
   return (
     <div className='leaguesWrapper'>
-        <p>Leagues</p>
+        {data.map((data) => (
+          <div key={data.id} className='leaugeDiv'>
+            <img src={data.logos.light} alt='club'/>
+            <h3>{data.name}</h3>
+          </div>
+        ))}
     </div>
   )
 }
